cd43a3ec2628c5d7740950c1ab0898d9e3f37626
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2021-01-31 17:29:41 +0330

    Ok, i'm done!!

--- src/algorithm/design/course/phase3/P3.java
+++ src/algorithm/design/course/phase3/P3.java
@@ -24,81 +24,62 @@ public class P3 {
         System.out.println(maxFlowMinimumCut(graph, 0, goal.i * n + goal.j + 1));
     }
 
+    private static boolean bfs(int[][] graph, int s, int t, int[] parent) {
 
-    private static boolean bfs(int[][] rGraph, int s, int t, int[] parent) {
+        boolean[] visited = new boolean[graph.length];
+        for (int i = 0; i < graph.length; ++i) visited[i] = false;
 
-        boolean[] visited = new boolean[rGraph.length];
-
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(s);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
         visited[s] = true;
         parent[s] = -1;
 
-        while (!q.isEmpty()) {
-            int v = q.poll();
-            for (int i = 0; i < rGraph.length; i++) {
-                if (rGraph[v][i] > 0 && !visited[i]) {
-                    q.offer(i);
-                    visited[i] = true;
-                    parent[i] = v;
+        while (queue.size() != 0) {
+            int u = queue.poll();
+
+            for (int v = 0; v < graph.length; v++) {
+                if (!visited[v] && graph[u][v] > 0) {
+                    queue.add(v);
+                    parent[v] = u;
+                    visited[v] = true;
                 }
             }
         }
         return visited[t];
     }
 
-    private static void dfs(int[][] rGraph, int s, boolean[] visited) {
-        visited[s] = true;
-        for (int i = 0; i < rGraph.length; i++) {
-            if (rGraph[s][i] > 0 && !visited[i]) {
-                dfs(rGraph, i, visited);
-            }
-        }
-    }
+    private static int maxFlowMinimumCut(int[][] graph, int source, int sink) {
 
-    private static int maxFlowMinimumCut(int[][] graph, int s, int t) {
+        int[][] copiedGraph = new int[graph.length][graph.length];
 
-        int u, v;
-
-        int[][] graphCopy = new int[graph.length][graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph.length; j++) {
-                graphCopy[i][j] = graph[i][j];
-            }
-        }
+        for (int i = 0; i < graph.length; i++)
+            System.arraycopy(graph[i], 0, copiedGraph[i], 0, graph.length);
 
         int[] parent = new int[graph.length];
 
-        while (bfs(graphCopy, s, t, parent)) {
+        int maxFlow = 0;
 
-            int pathFlow = Integer.MAX_VALUE;
-            for (v = t; v != s; v = parent[v]) {
-                u = parent[v];
-                pathFlow = Math.min(pathFlow, graphCopy[u][v]);
+        while (bfs(copiedGraph, source, sink, parent)) {
+
+            int path_flow = Integer.MAX_VALUE;
+            for (int i = sink; i != source; i = parent[i]) {
+                int p = parent[i];
+                path_flow = Math.min(path_flow, copiedGraph[p][i]);
             }
 
-            for (v = t; v != s; v = parent[v]) {
-                u = parent[v];
-                graphCopy[u][v] = graphCopy[u][v] - pathFlow;
-                graphCopy[v][u] = graphCopy[v][u] + pathFlow;
+            for (int i = sink; i != source; i = parent[i]) {
+                int p = parent[i];
+                copiedGraph[p][i] -= path_flow;
+                copiedGraph[i][p] += path_flow;
             }
-        }
 
-        boolean[] isVisited = new boolean[graph.length];
-        dfs(graphCopy, s, isVisited);
 
-        int res = 0;
-
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph.length; j++) {
-                if (graph[i][j] > 0 && isVisited[i] && !isVisited[j]) {
-                    System.out.println(i + " - " + j);
-                    if (i != 0) res += graph[i][j];
-                }
+            if (path_flow != Integer.MAX_VALUE) {
+//                System.out.println("Removed " + path_flow);
+                maxFlow += path_flow;
             }
         }
-
-        return res;
+        return maxFlow;
     }
 
     private static int[][] readInput(int m, int n) {
@@ -107,9 +88,16 @@ public class P3 {
         int[][] graph = new int[numberOfNodes][numberOfNodes];
 
 
-        for (int i = 1; i <= m * n; i++) {
-            graph[0][i] = Integer.MAX_VALUE;
+        for (int j = 1; j <= n; j++) {
+            graph[0][j] = Integer.MAX_VALUE; // i = 0
+            graph[0][(m - 1) * n + j] = Integer.MAX_VALUE; // i = m
         }
+
+        for (int i = 1; i < m; i++) {
+            graph[0][i * n + 1] = Integer.MAX_VALUE;  // j = 0
+            graph[0][i * n + n] = Integer.MAX_VALUE; // j = n
+        }
+
 
         int currentInNode = 1;
 
